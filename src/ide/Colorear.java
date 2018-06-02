8113604c23ba42099ac84c77ff2ece387901ad06
peper <peper@juan-HP-Pavilion-g7-Notebook-PC>
2018-06-02 01:03:44 -0500

    Colorear Completo pero hay errores de eficiencia

--- src/ide/Colorear.java
+++ src/ide/Colorear.java
@@ -8,11 +8,7 @@ package ide;
 
 
 import java.awt.Color;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JTextPane;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
@@ -25,20 +21,16 @@ import javax.swing.text.StyledDocument;
 public class Colorear {
     //Estados
     private static final int INICIO = 1;
-    private static final int ASIGNACION = 2;
-    private static final int MENORIGUAL = 3;
-    private static final int MAYORIGUAL = 4;
-    private static final int COMPARACION = 5;
-    private static final int CADENA = 6;
-    private static final int INDENTIFICADOR = 7;
-    private static final int DIGITO = 8;
-    private static final int POSIBLECOMMENT = 9;
-    private static final int POSIBLEDECIMAL = 10;
-    private static final int DIFERNETE = 11;
-    private static final int MULTICOMENT = 12;
-    private static final int INLINECOMMENT = 13;
-    private static final int POSIBLESALIDA = 14;
-    private static final int DECIMAL = 15;
+    private static final int ESPEROIGUAL = 4;
+    private static final int CADENA = 8;
+    private static final int INDETIFICADOR = 9;
+    private static final int DIGITO = 10;
+    private static final int POSIBLEDECIMAL = 11;
+    private static final int POSIBLECOMENT = 12;
+    private static final int INLINECOMENT = 13;
+    private static final int MULTIPLECOMENT = 15;
+    private static final int POSIBLESALIDA = 16;
+    private static final int DECIMAL = 17;
     //Colores
     private static final Color COLOR_ERROR = Color.RED;
     private static final Color COLOR_DIGIT = Color.cyan;
@@ -47,14 +39,15 @@ public class Colorear {
     private static final Color COLOR_CADENA = Color.ORANGE;
     private static final Color COLOR_COMMENT = Color.LIGHT_GRAY;
     private static final Color COLOR_PRESERVADA = Color.BLUE;
-
+    
+    String letra = "[a-zA-Z]";
     
     private String cadena;
     private int cadSize;
     private char caracter;
-    private final String[] PRESAERVADAS = {"main","if","then","else","end","do","while","repeat","until","read","write","float","integer","bool"};
-    private Style style;
-    private StyledDocument doc;
+    private final String[] PRESERVADAS = {"main","if","then","else","end","do","while","repeat","until","read","write","float","integer","bool"};
+    private final Style style;
+    private final StyledDocument doc;
     private int estadoActual;
     private int puntero;
     private int offsetaux;
@@ -67,8 +60,8 @@ public class Colorear {
         cadSize = this.cadena.length();
         caracter = ' ';
         style = tp.addStyle("Estilo", null);
-        estadoActual = INICIO;
         
+        estadoActual = INICIO;
         puntero=0;
         offsetaux = 0;
        
@@ -83,16 +76,21 @@ public class Colorear {
         
         cadena = doc.getText(0,doc.getLength());
         cadSize = this.cadena.length();
+        
         estadoActual = INICIO;
         puntero = 0;
         
         while(cadSize>puntero){
         
             caracter = cadena.charAt(puntero);
+            
             switch(estadoActual){
                 case INICIO: 
                     switch(caracter){
                         case ' ':
+                        case '\n':
+                        case '\t':
+                            break;
                         case ')':
                             setWord(puntero, puntero+1,COLOR_OPERADOR);
                             estadoActual = INICIO;
@@ -135,23 +133,24 @@ public class Colorear {
                             break;  
                         case '<'://como menor si existe solo lo pinto, pero igual reviso el siguente
                             setWord(puntero, puntero+1,COLOR_OPERADOR);
-                            estadoActual = MENORIGUAL;
+                            estadoActual = ESPEROIGUAL;
                             break;
                         case '>'://como menor si existe solo lo pinto, pero igual reviso el siguente
                             setWord(puntero, puntero+1,COLOR_OPERADOR);
-                            estadoActual = MAYORIGUAL;
+                            estadoActual = ESPEROIGUAL;
                             break;
                         case '!'://como menor si existe solo lo pinto, pero igual reviso el siguente
-                            estadoActual = DIFERNETE;
+                            estadoActual = ESPEROIGUAL;
                             break;
                         case '='://como menor si existe solo lo pinto, pero igual reviso el siguente
-                            estadoActual = COMPARACION;
+                            estadoActual = ESPEROIGUAL;
                             break;
                         case ':'://como menor si existe solo lo pinto, pero igual reviso el siguente
-                            estadoActual = ASIGNACION;
+                            estadoActual = ESPEROIGUAL;
                             break;
                         case '/':
-                            estadoActual = POSIBLECOMMENT;
+                            estadoActual = POSIBLECOMENT;
+                            offsetaux = puntero;
                             break;
                         case '\"':
                             estadoActual = CADENA;
@@ -159,168 +158,171 @@ public class Colorear {
                             break;
                         default:
                             if(Character.isDigit(caracter)){
+                                setWord(puntero,puntero+1,COLOR_DIGIT);
                                 estadoActual = DIGITO;
-                                offsetaux = puntero;
-                            }else if (!(caracter >= 'a' && caracter <= 'z') || !(caracter >= 'A' && caracter <= 'Z')){
-                               setWord(puntero, puntero+1,COLOR_IDENTIF);
-                               // estadoActual = INDENTIFICADOR;
-                                ///offsetaux = puntero;
+                            }else if (isCharacter(caracter)){
+                               //setWord(puntero, puntero+1,COLOR_IDENTIF);
+                               estadoActual = INDETIFICADOR;
+                               offsetaux = puntero;
                             }else{                                    
                                 setWord(puntero, puntero+1,COLOR_ERROR);
+                                //System.out.println("Error en Colorear");
                                 estadoActual = INICIO;
                             }
                             break;
                     }
                 break;
-            case MENORIGUAL: 
-                if(caracter=='='){
-                    setWord(puntero, puntero+1,COLOR_OPERADOR);
-                }else{
-                    puntero--;
-                }
-                estadoActual = INICIO;
-                break;
-            case MAYORIGUAL: 
-                if(caracter=='='){
-                    setWord(puntero, puntero+1,COLOR_OPERADOR);
-                }else{
-                    puntero--;
-                }
-                estadoActual = INICIO;
-                break;
-            case COMPARACION: 
+            case ESPEROIGUAL: 
                 if(caracter=='='){
                     setWord(puntero-1, puntero+1,COLOR_OPERADOR);
                 }else{
+                    if(cadena.charAt(puntero-1)==':'||cadena.charAt(puntero-1)=='='||cadena.charAt(puntero-1)=='!'){
+                        setWord(puntero-1, puntero,COLOR_ERROR);
+                    }
                     puntero--;
-                    setWord(puntero, puntero+1,COLOR_ERROR);
-                }
-                estadoActual = INICIO;
-                break;
-            case DIFERNETE: 
-                if(caracter=='='){
-                    setWord(puntero-1, puntero+1,COLOR_OPERADOR);
-                }else{
-                    puntero--;
-                    setWord(puntero, puntero+1,COLOR_ERROR);
-                }
-                estadoActual = INICIO;
-                break;
-            case ASIGNACION: 
-                if(caracter=='='){
-                    setWord(puntero-1, puntero+1,COLOR_OPERADOR);
-                }else{
-                    puntero--;
-                    setWord(puntero, puntero+1,COLOR_ERROR);
                 }
                 estadoActual = INICIO;
                 break;
             case CADENA: 
                 if(caracter=='\"'){
-                    setWord(offsetaux, puntero+1,COLOR_CADENA);
+                    setWord(offsetaux,puntero, COLOR_CADENA);
                     estadoActual = INICIO;
                 }
                 break;
-            case INDENTIFICADOR: 
-                if(caracter != '_' || !Character.isDigit(caracter)||!(caracter >= 'a' && caracter <= 'z')||!(caracter >= 'A' && caracter <= 'Z')){
-                    /*boolean bandaux=false;
-                    for(String actual:PRESAERVADAS){
-                        //if(doc.getText(offsetaux,puntero-1).equals(actual)){
-                        //    bandaux = true;
-                        //}
-                        //System.out.println(actual);
-                    }
-                    System.out.println(doc.getText(offsetaux,puntero));
-                    if(bandaux){
-                        //setWord(offsetaux, puntero,COLOR_PRESERVADA);
-                    }else{
-                        //setWord(offsetaux, puntero,COLOR_IDENTIF);
-                    }*/
-                    estadoActual = INICIO;
-                }
-                break;
-            case DIGITO: 
-                if(caracter=='.'){
-                    estadoActual = POSIBLEDECIMAL;
-                }else if(!Character.isDigit(caracter)){
-                    setWord(offsetaux,puntero+1,COLOR_DIGIT);
-                    puntero--;
-                    estadoActual = INICIO;
-                }
-                break;
-            case POSIBLEDECIMAL: 
-                if(!Character.isDigit(caracter)){
-                    setWord(offsetaux,puntero+1,COLOR_DIGIT);
-                    puntero-=1;
-                    estadoActual=INICIO;
-                }else{
-                    estadoActual = DECIMAL;
-                }
-                break;
-            case DECIMAL:
-                if(!Character.isDigit(caracter)){
-                    setWord(offsetaux,puntero,COLOR_DIGIT);
-                    puntero--;
-                    estadoActual = INICIO;
-                }
-            case POSIBLECOMMENT: 
-                switch (caracter) {
+            case POSIBLECOMENT:
+                    switch (caracter) {
                     case '/':
-                        estadoActual = INLINECOMMENT;
-                        offsetaux = puntero-1;
+                            estadoActual = INLINECOMENT;
                         break;
                     case '*':
-                        estadoActual = MULTICOMENT;
-                        offsetaux = puntero-1;
+                            estadoActual = MULTIPLECOMENT;
                         break;
                     default:
-                        setWord(puntero-1, puntero,COLOR_OPERADOR);
+                            setWord(puntero-1,puntero, COLOR_OPERADOR);
+                            estadoActual = INICIO;
+                            puntero--;
+                        break;    
+                    }
+                break;
+            case INLINECOMENT:
+                    if(caracter=='\n'){
+                        setWord(offsetaux, puntero-1,COLOR_COMMENT);
+                        estadoActual = INICIO;
+                    }
+                break;
+            case MULTIPLECOMENT:
+                    if(caracter=='*'){
+                        estadoActual = POSIBLESALIDA;
+                    }
+                break;
+            case POSIBLESALIDA:
+                    if(caracter=='/'){
+                        setWord(offsetaux, puntero, COLOR_COMMENT);
+                        estadoActual = INICIO;
+                    }else{
+                        estadoActual = MULTIPLECOMENT;
+                    }
+                break;
+            case DIGITO:
+                    if(caracter=='.'){
+                        estadoActual = POSIBLEDECIMAL;
+                        offsetaux = puntero;
+                    } else {
                         puntero--;
                         estadoActual = INICIO;
-                        break;
-                }
-                break;
-            case INLINECOMMENT:
-                if(caracter=='\n'){
-                    setWord(offsetaux,puntero,COLOR_COMMENT);
-                    estadoActual = INICIO;
-                }
-                break;
-            case MULTICOMENT: 
-                    if(caracter=='*'){
-                        estadoActual=POSIBLESALIDA;
                     }
                 break;
-            case POSIBLESALIDA: 
-                    if(caracter=='/'){
-                        setWord(offsetaux,puntero+1,COLOR_COMMENT);
-                        estadoActual=INICIO; 
+            case POSIBLEDECIMAL:
+                    if(!Character.isDigit(caracter)){
+                        setWord(offsetaux,offsetaux+1,COLOR_ERROR);
+                        estadoActual = INICIO;
                     }else{
-                        estadoActual=MULTICOMENT;
+                        estadoActual = DECIMAL;
                     }
+                break;
+            case DECIMAL:
+                    if(!Character.isDigit(caracter)){
+                        estadoActual = INICIO;
+                        puntero--;
+                    }
+                break;
+            case INDETIFICADOR:
+                if(!isCharacter(caracter) && caracter!='_' && !Character.isDigit(caracter)){
+                        
+                    String wordaux = cadena.substring(offsetaux, puntero);
+                    boolean bandaux = false;
+                        
+                    for (String PRESERVADAS1 : PRESERVADAS) {
+                        if (PRESERVADAS1.equals(wordaux)) {
+                            setWord(offsetaux, puntero, COLOR_PRESERVADA);
+                            bandaux = true;
+                            break;
+                        }
+                    }
+                    
+                    if(bandaux==false){
+                        setWord(offsetaux,puntero, COLOR_IDENTIF);
+                    }
+                         
+                    puntero--;
+                    estadoActual=INICIO;
+                }
                 break;
             }
+            
             puntero++;
         }
         
-        if(estadoActual==CADENA){
-            setWord(offsetaux,doc.getLength()-1,COLOR_CADENA);
-            System.out.println("Cadena Icompleta. con offset: " + offsetaux);
-        }else if(estadoActual==MULTICOMENT||estadoActual==POSIBLESALIDA){
-            setWord(offsetaux,doc.getLength()-1,COLOR_COMMENT);
-            System.out.println("Comentario Incompleto");
+        switch (estadoActual) {
+            case CADENA:
+                setWord(offsetaux,doc.getLength()-1,COLOR_CADENA);
+                break;
+            case POSIBLECOMENT:
+            case MULTIPLECOMENT:
+            case INLINECOMENT:
+                setWord(offsetaux,doc.getLength()-1,COLOR_COMMENT);
+                break;
+            case POSIBLEDECIMAL:
+            case ESPEROIGUAL:
+                if(cadena.charAt(puntero-1)!='<'&&cadena.charAt(puntero-1)!='>'){
+                    setWord(puntero-1,puntero, COLOR_ERROR);
+                }
+                break;
+            case INDETIFICADOR:
+                    String wordaux = cadena.substring(offsetaux, puntero);
+                    boolean bandaux = false;
+                    for (String PRESERVADAS1 : PRESERVADAS) {
+                        if (PRESERVADAS1.equals(wordaux)) {
+                            setWord(offsetaux, puntero, COLOR_PRESERVADA);
+                            bandaux = true;
+                            break;
+                        }
+                    }
+                    if(bandaux==false){
+                        setWord(offsetaux,puntero, COLOR_IDENTIF);
+                    }
+                break;
+            default:
+                break;
         }
-        
+            
+        System.out.println("Estado Final es:" + estadoActual);
     }
     
     public void setWord(int ini, int fin, Color c) {
         StyleConstants.setForeground(style, c);
-        if(fin<doc.getLength()){
+        if(fin<=doc.getLength()){
             doc.setCharacterAttributes(ini, fin, style, true);
+        }else{
+            System.out.println("Se intenta pintar en un carater fuera del indice");
         }
     }
     
-    public void agregarellistener(){
+    private static boolean isCharacter(char aux){
+        return (aux >= 'a' && aux <= 'z') || (aux >= 'A' && aux <= 'Z');
+    }
+    
+    /*public void agregarellistener(){
         doc.addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
@@ -342,16 +344,5 @@ public class Colorear {
                 
             }
         });
-    }
-    
-    /*private void highlight() {
-
-    Runnable doHighlight = new Runnable() {
-        @Override
-        public void run() {
-             colorear();
-            }
-        };       
-        SwingUtilities.invokeLater(doHighlight);
     }*/
 }
